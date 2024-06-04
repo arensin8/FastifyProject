2223b02daf7b423d992600faf3ed5152614efb1b
arensin8 <arensinani4@gmail.com>
2024-06-04 14:19:38 +0400

    router  configs

---
+++ product.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    name: "notebbok",
+  },
+  {
+    id: 2,
+    name: "pen",
+  },
+  {
+    id: 3,
+    name: "pencil",
+  },
+  {
+    id: 4,
+    name: "eraser",
+  },
+];
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-const fastify = require("fastify")({
+import { products } from "./product.js";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
   logger: true,
 });
 
 const PORT = 5000;
+
+fastify.get("/", (req, reply) => {
+  reply.send({
+    message: "Hello world",
+  });
+});
+
+fastify.get("/products", (req, reply) => {
+  reply.send(products);
+});
+
+fastify.get("/products/:id", (req, reply) => {
+  const { id } = req.params;
+  const product = products.find((p) => p.id == id);
+  if (!product) reply.code(404).send("not found!");
+  reply.send(product);
+});
 
 const main = async () => {
   try {
