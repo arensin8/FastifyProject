4f566ccb5a77e48bb7c7badef069173b3db70d92
arensin8 <arensinani4@gmail.com>
2024-06-10 13:53:21 +0400

    creating models

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -1,8 +1,16 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  "postgres://postgres:arensin2002@localhost:5433/fastify"
-);
+//fisrt method for connecting to db
+// export const sequelize = new Sequelize(
+//   "postgres://postgres:arensin2002@localhost:5433/fastify"
+// );
+
+//second method for connecting to db
+export const sequelize = new Sequelize("fastify", "postgres", "arensin2002", {
+  host: "localhost",
+  port: 5433,
+  dialect: "postgres",
+});
 
 const DBconnection = async () => {
   await sequelize.authenticate();
--- model/user.model.js
+++ model/user.model.js
@@ -1,3 +1,42 @@
+import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/sequelize.config";
 
-sequelize
+export class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    birthday: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    name: "users",
+  }
+);
+
+User.sync({ force: true });
