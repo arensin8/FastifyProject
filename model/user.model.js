760afedb87095b1c861e6ebdb9656db16a8389c2
arensin8 <arensinani4@gmail.com>
2024-06-19 16:27:01 +0400

    creating new tables

--- model/user.model.js
+++ model/user.model.js
@@ -1,46 +1,69 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/sequelize.config.js";
 
-export class User extends Model {}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    first_name: {
-      type: DataTypes.STRING,
-    },
-    last_name: {
-      type: DataTypes.STRING,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    birthday: {
-      type: DataTypes.DATE,
-    },
-    accessToken: {
-      type: DataTypes.STRING,
-      defaultValue: "",
-    },
+//User table
+export const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    sequelize,
-    name: "users",
-  }
-);
+  first_name: {
+    type: DataTypes.STRING,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  birthday: {
+    type: DataTypes.DATE,
+  },
+  accessToken: {
+    type: DataTypes.STRING,
+    defaultValue: "",
+  },
+});
 
-// User.sync({ force: true });
+//userDetails table
+export const UserDetails = sequelize.define("UserDetails", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+  latitudes: {
+    type: DataTypes.DOUBLE,
+  },
+  longitudes: {
+    type: DataTypes.DOUBLE,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+  },
+});
+
+User.hasOne(UserDetails);
+UserDetails.belongsTo(User);
+
+// For managing that , when we change sth in db it updates automaticlly
+// User.sync({ alter: true }).then(() => {
+//   console.log("user sync completed");
+// });
+// UserDetails.sync({ alter: true }).then(() => {
+//   console.log("userDetails sync completed");
+// });
